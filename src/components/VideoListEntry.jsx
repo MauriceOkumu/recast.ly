8a9140710a02bc2036c117336e9342b44a164366
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-01-07 04:00:44 -0800

    VideolistEntry tests passing

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,10 +1,11 @@
-var VideoListEntry = ({video}) => (
+var VideoListEntry = ({video,onClickHandle}) => (
   <div className="video-list-entry">
     <div className="media-left media-middle">
       <img className="media-object" src={video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{video.snippet.title}</div>
+      <div className="video-list-entry-title"
+      onClick={() => onClickHandle(video)}>{video.snippet.title}</div>
       <div className="video-list-entry-detail">{video.snippet.description}</div>
     </div>
   </div>
